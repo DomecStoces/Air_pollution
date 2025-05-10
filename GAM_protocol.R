6252597cc9d1bc70db86dc520886cc1b40851a6b
Dominik Stočes <xstoces@mendelu.cz>
2025-05-10 23:32:34 +0200

    check for family

--- GAM_protocol.R
+++ GAM_protocol.R
@@ -5,3 +5,11 @@ library(gratia) #ggplot like visualization of estimated smooths
 
 gam(Abundance ~ s(year) + s(SO2) + ti(year, SO2) + s(WoodySpecies, bs = "re"), 
     data = df, family = nb(), method = "REML")
+
+# Poisson
+m_pois <- gam(Abundance ~ s(year) + s(SO2) + ti(year, SO2), 
+              data = format1, family = poisson(), method = "REML")
+
+# Negative Binomial (theta estimated automatically)
+m_nb <- gam(Abundance ~ s(year) + s(SO2) + ti(year, SO2), 
+            data = format1, family = nb(), method = "REML")
