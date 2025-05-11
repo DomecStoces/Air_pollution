5240b1192004d54dec657b88af03dd30ceeef926
Dominik Stočes <xstoces@mendelu.cz>
2025-05-11 10:52:29 +0200

    changes to basis dimensions.

--- GAM_protocol.R
+++ GAM_protocol.R
@@ -10,6 +10,7 @@ format1$Woody.species <- as.factor(format1$Woody.species)
 #####
 #Always check concurvity (mgcv::concurvity()), and plot interactions using vis.gam() or gratia::draw()
 
+#####
 #Check for better model fit of family()
 #####
 # Poisson model
@@ -32,18 +33,27 @@ disp_nb <- sum(residuals(m_nb, type = "pearson")^2) / df.residual(m_nb)
 
 disp_pois
 disp_nb
-
+#####
 #Modelling
+#Final model fit
 #####
 #Smooth interaction using tensor product smooth
-fit1<-gam(Number ~ s(Time.period, k = 15) + s(T, k = 12) + s(Precipitation, k = 12) + ti(Time.period, Immission, k = c(15, 10)) + s(Woody.species, bs = "re"), data = format1, family = nb(), method = "REML")
-
+fit1<-gam(Number ~ s(Time.period, k = 12) + s(T, k = 13) + s(Precipitation, k = 15) + ti(Time.period, Immission, k = c(20, 15)) + s(Woody.species, bs = "re"), data = format1, family = nb(), method = "REML")
 #Check concurvity
 concurvity(fit1)
-#Residual diagnosis
+#Residual diagnosis k-index >= 1 (setting for model); basis dimensions were checked and tuned.
 gam.check(fit1)
 #Visualization
-draw(fit1, select = "ti(Time.period,Immission)")
+plot(fit1, select = 4, scheme = 2, 
+     shade = TRUE, 
+     main = "Interaction: Time.period × Immission", 
+     xlab = "Time.period", 
+     ylab = "Immission")
+
+draw(fit1, select = "ti(Time.period,Immission)", 
+     contour = TRUE) +
+  ggtitle("Tensor product smooth: Time × Immission") +
+  xlab("Time.period") + ylab("Immission")
 
 vis.gam(fit1,
         view = c("Time.period", "Immission"),
@@ -62,15 +72,25 @@ vis.gam(fit1,
         phi = 30,
         ticktype = "detailed",
         main = "Interaction: Time × SO₂ Immission")
-
+#####
+fit_linear_time <- gam(Number ~ Time.period + 
+                         s(T) + 
+                         s(Precipitation) + 
+                         ti(Time.period, Immission) + 
+                         s(Woody.species, bs = "re"),
+                       data = format1, family = nb(), method = "ML")
 #Different model options of 'Immission' term:
-
+#####
 fit2<-gam(Number ~ s(Time.period) + s(T) + s(Precipitation) + s(Immission)+ ti(Time.period, Immission) + s(Woody.species, bs = "re"), data = format1, family = nb(), method = "ML")
 
 fit3<-gam(Number ~ s(Time.period) + s(T) + s(Precipitation) + ti(Time.period, Immission) + s(Woody.species, bs = "re"), data = format1, family = nb(), method = "ML")
 
 fit4<-gam(Number ~ s(Time.period) + ti(Time.period, Immission) + s(Woody.species, bs = "re"), data = format1, family = nb(), method = "ML")
-AIC(fit2,fit4)
-anova(fit2, fit4, test = "Chisq")
+
+fit5<-gam(Number ~ s(T) + s(Precipitation) + s(Immission) + ti(Time.period, Immission) + s(Woody.species, bs = "re"), data = format1, family = nb(), method = "ML")
+AIC(fit2,fit5)
+anova(fit2, fit5, test = "Chisq")
 #it is better to stick with 'Immission' as interaction term only
 #it is better to use model with the Temperature and Precipitation; and Immission in interaction term with Time period
+#there is an independent temporal trend in data (e.g., seasonal or long-term effects)
+#that trend is further modulated by Immission, captured by the interaction.
