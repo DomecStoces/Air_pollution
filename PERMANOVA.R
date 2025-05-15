8824ce4c120d26c15e316cc378d5db85f04b747c
DomecStoces <dominik.stoces@mendelu.cz>
2025-05-15 12:27:08 +0200

    setting for Birch

--- PERMANOVA.R
+++ PERMANOVA.R
@@ -1,32 +1,20 @@
 #PERMANOVA: to test global effects
 #How community composition (species assemblages divided into life-history traits) varies with SO₂ and year?
 #Are the species identities and abundances different between time periods?
+#I noticed that I cannot do PERMANOVA for this dataset, because there are no true replicates, only for Birch = Woody.species
 
 library(vegan)
 library(dplyr)
 library(tidyr)
 library(readxl)
 
-format1 <- format1 %>%
-  mutate(Date = as.Date(Date))
-format1 <- format1 %>%
-  mutate(
-    PolicyPeriod = case_when(
-      Date < as.Date("1991-10-04") ~ "Pre1991",
-      Date >= as.Date("1991-10-04") & Date < as.Date("2002-06-01") ~ "1991_2002",
-      Date >= as.Date("2002-06-01") & Date < as.Date("2012-09-01") ~ "2002_2012",
-      Date >= as.Date("2012-09-01") ~ "Post2012"
-    )
-  )
-
 #Create a unique sample ID for grouping variable
 format1 <- format1 %>%
   mutate(SampleID = paste(Time.period, Locality, Woody.species, sep = "_"))
 
 #Create species matrix: rows = samples, columns = species, values = total abundance
-sp_matrix <- format1 %>%
-  group_by(SampleID, Species) %>%
-  summarise(Abundance = sum(Number), .groups = "drop") %>%
+sp_matrix <- format1 %>% group_by(Woody.species, Species)
+summarise(Abundance = sum(Number)) %>%
   pivot_wider(names_from = Species, values_from = Abundance, values_fill = 0)
 #####
 #Convert to matrix
@@ -69,3 +57,5 @@ print(adonis_result3)
 adonis_result2<-adonis2(bray_dist ~ PolicyPeriod*Immission, data = env_data, permutations = 1, method = "bray",by=NULL)
 
 adonis_result3<-adonis2(bray_dist ~ PolicyPeriod+Immission, data = env_data, permutations = 1)
+
+
